31b2a70d86a39cea7c49d3a1a3e379efe76ec67f
Vinicius Martinez <vn.martinez@hotmail.com>
2024-06-08 22:08:49 -0300

    uma hora vai

--- main.py
+++ main.py
@@ -58,4 +58,4 @@ def listar_item(item_id: int):
     return item
     
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=os.getenv("PORT", default=5000), log_level="info")
+    uvicorn.run("main:app", host="0.0.0.0", port=int(os.getenv("PORT", default=5000)), log_level="info")
